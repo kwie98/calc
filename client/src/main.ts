1f1f7205d5d9a9afaed041c032a061f7f96691de
Konrad Wienecke <konrad.wienecke@gmail.com>
2023-12-15 16:50:17 +0100

    Add simple calculator view

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,24 +1,17 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import "./style.css";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+let current_equation = "";
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const numbers = document.querySelectorAll<HTMLButtonElement>(".number")
+for (const n of numbers) {
+    console.log(n.innerHTML)
+}
+
+const disp = document.querySelector<HTMLInputElement>(".disp")
+// disp?.oninput = (e) => {
+//     console.log(e)
+// }
+disp?.addEventListener("input", (e) => {
+    console.log(e)
+    // e.preventDefault()
+})
