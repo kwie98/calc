bbc1fd5f673fa81be5c16af47fc1ea7a6dcf5a2e
Konrad Wienecke <konrad.wienecke@gmail.com>
2023-12-18 12:26:42 +0100

    Refactor tests

--- tests/tokenize.test.ts
+++ tests/tokenize.test.ts
@@ -1,4 +1,4 @@
-import { expect, test } from "vitest";
+import { expect, it } from "vitest";
 import { Token, tokenize } from "../src/tokenize";
 
 function untokenize(tokens: Token[]): string {
@@ -6,52 +6,53 @@ function untokenize(tokens: Token[]): string {
 }
 
 // biome-ignore format:
-{
+it("untokenizes correctly", () => {
     const tokens = ["(", "-312316", "*", "(", "+4256.1", "+", -590, "-", "+612", ")", ")", "/", -7234];
-    test("untokenizes correctly", () =>
-        expect(untokenize(tokens)).toEqual("(-312316*(+4256.1+-590-+612))/-7234"));
-}
+    return expect(untokenize(tokens)).toEqual("(-312316*(+4256.1+-590-+612))/-7234")
+});
 
-test("tokenizes empty string", () => expect(tokenize("")).toEqual([]));
-test("tokenizes positive number", () => expect(tokenize("+4")).toEqual([4]));
-test("tokenizes equation with a positive and negative number", () =>
+it("tokenizes empty string", () => expect(tokenize("")).toEqual([]));
+
+it("tokenizes positive number", () => expect(tokenize("+4")).toEqual([4]));
+
+it("tokenizes equation with a positive and negative number", () =>
     expect(tokenize("+3+-3")).toEqual([3, "+", -3]));
-test("errors on wrong equation", () =>
+
+it("errors on wrong equation", () =>
     expect(() => tokenize("+-")).toThrowError());
-{
+
+it("tokenizes number", () => {
     const tokens = [12345];
-    test("tokenizes number", () =>
-        expect(tokenize(untokenize(tokens))).toEqual(tokens));
-}
-{
+    return expect(tokenize(untokenize(tokens))).toEqual(tokens);
+});
+
+it("tokenizes negative number", () => {
     const tokens = [-23];
-    test("tokenizes negative number", () =>
-        expect(tokenize(untokenize(tokens))).toEqual(tokens));
-}
-{
+    return expect(tokenize(untokenize(tokens))).toEqual(tokens);
+});
+
+it("tokenizes simple equation", () => {
     const tokens = [1, "-", 2];
-    test("tokenizes simple equation", () =>
-        expect(tokenize(untokenize(tokens))).toEqual(tokens));
-}
-{
+    return expect(tokenize(untokenize(tokens))).toEqual(tokens);
+});
+
+it("tokenizes - + equation", () => {
     const tokens = [4, "-", 5, "+", 6];
-    test("tokenizes - + equation", () =>
-        expect(tokenize(untokenize(tokens))).toEqual(tokens));
-}
-{
+    return expect(tokenize(untokenize(tokens))).toEqual(tokens);
+});
+
+it("tokenizes + - equation", () => {
     const tokens = [4, "+", 5, "-", 6];
-    test("tokenizes + - equation", () =>
-        expect(tokenize(untokenize(tokens))).toEqual(tokens));
-}
-{
-    // biome-ignore format:
+    return expect(tokenize(untokenize(tokens))).toEqual(tokens);
+});
+
+// biome-ignore format:
+it("tokenizes complex equation", () => {
     const tokens = ["(", -312316, "*", "(", 4256.1, "+", -590, "-", 612, ")", ")", "/", -7234];
-    test("tokenizes complex equation", () => {
-        expect(tokenize(untokenize(tokens))).toEqual(tokens);
-    });
-}
-{
-    const tokens = ["(", ")"];
-    test("tokenizes bracket-only equation", () =>
-        expect(tokenize(untokenize(tokens))).toEqual(tokens));
-}
+    expect(tokenize(untokenize(tokens))).toEqual(tokens);
+});
+
+const tokens = ["(", ")"];
+it("tokenizes bracket-only equation", () => {
+    return expect(tokenize(untokenize(tokens))).toEqual(tokens);
+});
