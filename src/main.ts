f45399632b611488d48c0b4b0dc43c21867218cd
Konrad Wienecke <konrad.wienecke@gmail.com>
2023-12-19 22:43:32 +0100

    Handle annoying 0 in display

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,34 @@
 import { lex } from "./lex";
 import { resolvePostfix, toPostfix } from "./postfix";
 
+const DEFAULT_DISPLAY = "0";
+
 const display = document.querySelector("#display") as HTMLInputElement;
 
-const charBtns = document.querySelectorAll<HTMLButtonElement>(".char");
-for (const btn of charBtns) {
+const symbolBtns = document.querySelectorAll<HTMLButtonElement>(".symbol");
+for (const btn of symbolBtns) {
     btn.onclick = () => {
         display.value = display.value + btn.innerHTML;
     };
 }
 
+const numberBtns = document.querySelectorAll<HTMLButtonElement>(".number");
+for (const btn of numberBtns) {
+    btn.onclick = () => {
+        display.value =
+            display.value === DEFAULT_DISPLAY
+                ? btn.innerHTML
+                : display.value + btn.innerHTML;
+    };
+}
+
 const del = document.querySelector("#delete") as HTMLButtonElement;
 del.onclick = () => {
-    display.value = "0"
-}
+    display.value = DEFAULT_DISPLAY;
+};
 
 const equals = document.querySelector("#equals") as HTMLButtonElement;
 equals.onclick = () => {
-    const input = display.value.length === 0 ? "0" : display.value
-    display.value = resolvePostfix(toPostfix(lex(input))).toString()
-}
+    const input = display.value.length === 0 ? DEFAULT_DISPLAY : display.value;
+    display.value = resolvePostfix(toPostfix(lex(input))).toString();
+};
